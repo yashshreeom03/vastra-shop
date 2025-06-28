a37b1f80425cc4e81eb424d13c9c00ade9117f3a
gajeray5 <153584555+gajeray5@users.noreply.github.com>
2025-06-28 18:00:31 +0530

    update vastra shop code

--- assets/js/main.js
+++ assets/js/main.js
@@ -94,14 +94,17 @@ $(document).ready(function(){
       autoplaySpeed: 8000,
       responsive: [
         {
+          slidesToScroll: 3,
           breakpoint: 1024,
           settings: { slidesToShow: 3 }
         },
         {
+          slidesToScroll: 2,
           breakpoint: 768,
           settings: { slidesToShow: 2 }
         },
         {
+          slidesToScroll: 1,
           breakpoint: 480,
           settings: { slidesToShow: 1 }
         }
