8f09f64d6e68705b1840128307b0295e56260bb2
gajeray5 <153584555+gajeray5@users.noreply.github.com>
2025-06-28 18:03:56 +0530

    update vastra shop code

--- assets/js/main.js
+++ assets/js/main.js
@@ -83,31 +83,37 @@ $(document).ready(function(){
     });
   });
 
-    $(document).ready(function(){
-    $('.designer-slick').slick({
-      dots: true,
-      infinite: true,
-      arrows: false,
-      speed: 300,
-      slidesToShow: 4,
-      slidesToScroll: 4,
-      autoplaySpeed: 8000,
-      responsive: [
-        {
-          slidesToScroll: 3,
-          breakpoint: 1024,
-          settings: { slidesToShow: 3 }
-        },
-        {
-          slidesToScroll: 2,
-          breakpoint: 768,
-          settings: { slidesToShow: 2 }
-        },
-        {
-          slidesToScroll: 1,
-          breakpoint: 480,
-          settings: { slidesToShow: 1 }
+$(document).ready(function(){
+  $('.designer-slick').slick({
+    dots: true,
+    infinite: true,
+    arrows: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    autoplaySpeed: 8000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3
         }
-      ]
-    });
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   });
+});
